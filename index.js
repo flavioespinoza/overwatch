a8106a599436f5cfaa4b554477188f624093d78a
flavioespinoza <flavio.espinoza@gmail.com>
2021-09-02 15:26:41 -0600

    websocket working

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 7000;
+const {
+  CoinbasePro,
+  WebSocketChannelName,
+  WebSocketEvent,
+} = require('coinbase-pro-node');
+const client = new CoinbasePro();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// 2. Setup WebSocket channel info
+const channel = {
+  name: WebSocketChannelName.TICKER,
+  product_ids: ['BTC-USD'],
+};
+
+// 3. Wait for open WebSocket to send messages
+client.ws.on(WebSocketEvent.ON_OPEN, () => {
+  // 7. Subscribe to WebSocket channel
+  client.ws.subscribe([channel]);
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+// 4. Listen to WebSocket subscription updates
+client.ws.on(WebSocketEvent.ON_SUBSCRIPTION_UPDATE, (subscriptions) => {
+  // When there are no more subscriptions...
+  if (subscriptions.channels.length === 0) {
+    // 10. Disconnect WebSocket (and end program)
+    // client.ws.disconnect();
+  }
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running on port: ${port}`);
+// 5. Listen to WebSocket channel updates
+client.ws.on(WebSocketEvent.ON_MESSAGE_TICKER, (msg) => {
+  // 8. Receive message from WebSocket channel
+  console.info(msg);
+
+  // 9. Unsubscribe from WebSocket channel
+  // client.ws.unsubscribe([
+  //   {
+  //     name: WebSocketChannelName.TICKER,
+  //     product_ids: [tickerMessage.product_id],
+  //   },
+  // ]);
 });
+
+// 6. Connect to WebSocket
+client.ws.connect({ debug: true });
